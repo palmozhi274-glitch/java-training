c36d6610232674f4d3f5223a1f352b5e80c301dd
palmozhi <palmozhi274@gmail.com>
2026-07-23 17:21:04 +0530

    Initial

---
+++ Day29/clonegraph.java
@@ -0,0 +1,27 @@
+package Day29;
+
+
+import java.util.*;
+
+class Solution {
+
+    private Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+
+        if (node == null)
+            return null;
+
+        if (map.containsKey(node))
+            return map.get(node);
+
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clone;
+    }
+}
